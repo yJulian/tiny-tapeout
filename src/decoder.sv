718f58fac00e7275c096ee3a5f6aba359906f2be
Julian Leitner <yjulian@outlook.de>
2026-03-15 14:10:43 +0100

    Cleanup

--- src/decoder.sv
+++ src/decoder.sv
@@ -5,7 +5,6 @@ module decoder (
     output logic peri_en,
     output logic [2:0] imm
 );
-    // Kombinatorisch, damit Steuersignale sofort anliegen
     always_comb begin
         case (op[4:3])
             2'b00: begin    // ALU OP
@@ -14,19 +13,19 @@ module decoder (
                 peri_en = 1'b0;
                 imm     = op[2:0];
             end
-            2'b01: begin    // MEM OP (Load ui_in to Reg)
+            2'b01: begin    // MEM OP
                 we      = 1'b1;
                 alu_en  = 1'b0;
                 peri_en = 1'b0;
                 imm     = op[2:0];
             end
-            2'b10: begin    // DATAFLOW OP (Config Periphery)
+            2'b10: begin    // DATAFLOW OP
                 we      = 1'b0;
                 alu_en  = 1'b0;
                 peri_en = 1'b1;
                 imm     = op[2:0];
             end
-            default: begin
+            default: begin  // DEFAULT
                 we      = 1'b0;
                 alu_en  = 1'b0;
                 peri_en = 1'b0;
